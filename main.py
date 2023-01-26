592e1bb5430413896bcd2116358642cc0d0afd24
Carlos-GitH <123604173+Carlos-GitH@users.noreply.github.com>
2023-01-25 21:34:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ while game_is_on:
         scoreboard.increase_score()
 
     #Detect collision with wall.
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() > 300 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -300:
         scoreboard.reset()
         snake.reset()
 
